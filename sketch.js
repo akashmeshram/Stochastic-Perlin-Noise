b567acacd5c6fb6508267dbe3ebad37bd0101e35
Akash Meshram <6388495akash@gmail.com>
2022-02-22 21:29:11 +0530

    🏗️ Code Refactor

--- sketch.js
+++ sketch.js
@@ -1,84 +1,86 @@
-let total = 80;
-let scale = 600;
+const TOTAL_PARTICLES = 50;
+const SCALE = 600;
 
-let PartOne = new Array(total);
-let PartTwo = new Array(total);
-let PartThree = new Array(total);
-let taken;
+const ALL_COLORS = [
+  { r: 227, g: 21, b: 12 },
+  { r: 21, g: 83, b: 157 },
+  { r: 6, g: 190, b: 194 },
+  { r: 221, g: 132, b: 69 },
+  { r: 14, g: 197, b: 50 },
+  { r: 190, g: 71, b: 33 },
+];
+
+let AllCurves;
+let board;
+
+class Particle {
+  constructor(position) {
+    this.pos = position;
+    this.speed = 0.4;
+  }
+  update() {
+    const noiseScale =
+      noise(this.pos.x / SCALE, this.pos.y / SCALE) * TWO_PI * SCALE;
+    const veclocity = createVector(cos(noiseScale), sin(noiseScale)).mult(
+      this.speed
+    );
+    this.pos.add(veclocity);
+  }
+  display() {
+    ellipse(this.pos.x, this.pos.y, 2, 2);
+  }
+}
+
+class Curve {
+  constructor({ r, g, b }) {
+    this.curveColor = color(r, g, b);
+    this.collection = [];
+  }
+  addParticle(particle) {
+    this.collection = [...this.collection, particle];
+  }
+  createCurve() {
+    fill(this.curveColor);
+    this.collection.map((item) => {
+      item.update();
+      item.display();
+    });
+  }
+}
+
+class Board {
+  constructor(height, width) {
+    this.board = [...new Array(height)].map((_) =>
+      [...new Array(width)].map((__) => false)
+    );
+  }
+  generateParticle() {
+    let position;
+    do {
+      position = createVector(floor(random(width)), floor(random(height)));
+    } while (this.check(position.x, position.y));
+    this.active(position.x, position.y);
+    return new Particle(position);
+  }
+  check(x, y) {
+    return this.board[y][x];
+  }
+  active(x, y) {
+    this.board[y][x] = true;
+  }
+}
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    background(255,245,204);
-    noStroke();
-    taken = new Array(height);
-
-    for(let j = 0; j < height; j++){
-        taken[j] = new Array(width);
-    }
-
-    for(let j = 0; j < height; j++){
-        for(let i = 0; i < width; i++){
-            taken[j][i] = false;
-        }
-    }
-
-    for (let i = 0; i < total; i++) {
-        PartOne[i] = new Particle();
-    }
-    for (let i = 0; i < total; i++) {
-        PartTwo[i] = new Particle();
-    }
-    for (let i = 0; i < total; i++) {
-        PartThree[i] = new Particle();
-    }
-
+  createCanvas(windowWidth, windowHeight);
+  background(5, 6, 5);
+  noStroke();
+  board = new Board(height, width);
+  AllCurves = ALL_COLORS.map((color) => new Curve(color));
+  [...new Array(TOTAL_PARTICLES)].map((_) =>
+    AllCurves.map((curve) => curve.addParticle(board.generateParticle()))
+  );
 }
 
 function draw() {
-
-    for (let i of PartOne) {
-        fill(255,65,98);
-        i.update();
-        i.display();
-    }
-    for (let i of PartTwo) {
-        fill(23,222,238);
-        i.update();
-        i.display();
-    }
-    for (let i of PartThree) {
-        fill(0,244,0);
-        i.update();
-        i.display();
-    }
-}
-
-class Particle {
-    constructor() {
-
-        do {
-          this.pos = createVector(floor(random(width)), floor(random(height)));
-        } while(taken[this.pos.y][this.pos.x]);
-
-        taken[this.pos.y][this.pos.x] = true;
-
-
-        this.speed = 0.4;
-    }
-
-    update() {
-        let scl = noise(this.pos.x / scale, this.pos.y / scale) * TWO_PI * scale;
-        let vel = createVector(cos(scl), sin(scl)).mult(this.speed);
-        this.pos.add(vel);
-    }
-
-    display() {
-        ellipse(this.pos.x, this.pos.y, 2, 2);
-    }
-
-    edges() {
-        if (this.pos.x < 0 || this.pos.x > width || this.pos.y < 0 || this.pos.y > height) {
-            this.pos = createVector(random(width), random(height));
-        }
-    }
+  AllCurves.map((curve) => curve.createCurve());
 }
